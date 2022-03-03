984a612bd17d1e15af090c000ce86453a376a031
jakubbedra <jakubbedra@wp.pl>
2022-03-03 22:40:49 +0100

    code change

--- src/test/java/pl/edu/pg/eti/bsk/filetransferer/data/EncryptionTest.java
+++ src/test/java/pl/edu/pg/eti/bsk/filetransferer/data/EncryptionTest.java
@@ -1,16 +1,37 @@
 package pl.edu.pg.eti.bsk.filetransferer.data;
 
 import org.junit.Test;
+import pl.edu.pg.eti.bsk.filetransferer.logic.AesEncryption;
 
-import javax.crypto.Cipher;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class EncryptionTest {
 
     @Test
-    public void encryptionSample() {
+    public void encryptionSample()
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        String input = "sample text";
+        String algorithm = "AES/CBC/PKCS5Padding";
+        IvParameterSpec iv = AesEncryption.generateIv();
+        SecretKey key = AesEncryption.generateKey(128);
 
-        Cipher.getInstance("");
-
+        byte[] ciphertext = AesEncryption.encryptAes(
+                algorithm, input.getBytes(StandardCharsets.UTF_8), key, iv
+        );
+        byte[] plaintext = AesEncryption.decryptAes(
+                algorithm, ciphertext, key, iv
+        );
+        System.out.println(new String(ciphertext));
+        System.out.println(new String(plaintext));
     }
 
 }
